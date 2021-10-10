3324d33d7e30a6e15d57babef91ad72620e32a6a
makssein <90086818+makssein@users.noreply.github.com>
2021-10-10 23:35:36 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "func.h"
+
+
+
+int main() {
+    char s[100];
+    char small[100];
+    char big[100];
+    char maxSymbol;
+    int symbols[256];
+    int maxsymb = 0;
+
+    myFunc::Read(s);
+    myFunc::fromBigToSmall(s, small);
+    myFunc::fromSmallToBig(s, big);
+    myFunc::maxSymbol(symbols, maxsymb, maxSymbol, s);
+
+
+
+
+    std::cout << big << ":" << small << ":" << maxSymbol;
+    return 0;
+}
