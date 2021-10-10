5a2e41c3a3d5507665d35c7fc1fa4acfedef30d4
makssein <90086818+makssein@users.noreply.github.com>
2021-10-10 23:35:59 +0200

    Create func.h

---
+++ func.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace myFunc{
+    void Read(char s[100]);
+    char fromBigToSmall(char s[100], char small[100]);
+    char fromSmallToBig(char s[100], char big[100]);
+    char maxSymbol(int symbols[256], int maxsymb, char& maxSymbol, char s[100]);
+}
